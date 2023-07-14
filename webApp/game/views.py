d64dc3ad86accdf7064c2f8257e4a5c5557098af
Hassan <hassansmallah@gmail.com>
2023-07-14 23:37:30 +0200

    Bootstrap - index

--- webApp/game/views.py
+++ webApp/game/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse, HttpRequest
+from django.template import loader
 
 
 # Create your views here.
 
 def index(request: HttpRequest):
-    return HttpResponse("Hello, world!")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({}))
