3e713385f17771f964cbe2908bb755aea85361d4
Hassan <hassansmallah@gmail.com>
2023-07-16 14:30:48 +0200

    post request

--- webApp/game/views.py
+++ webApp/game/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse, HttpRequest
 from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 
+@csrf_exempt
 def index(request: HttpRequest):
+    print(request.POST)
+
     template = loader.get_template('index.html')
     return HttpResponse(template.render({}))
