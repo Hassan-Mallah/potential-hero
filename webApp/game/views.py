230998d8bbdace575bee105e55ac3ac4d3dcccb7
Hassan <hassansmallah@gmail.com>
2023-07-20 20:54:41 +0200

    Welcome message

--- webApp/game/views.py
+++ webApp/game/views.py
@@ -7,11 +7,15 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def index(request: HttpRequest):
-    print(request.POST)
-    context = {
-        'question': 'Which weapon would do you like to use?',
-        'answers': ['Axe', 'Gun', 'Sword']
-    }
-
     template = loader.get_template('index.html')
+    context = {}
+
+    if request.POST:
+        context = {
+            'question': 'Which weapon would do you like to use?',
+            'answers': ['Axe', 'Gun', 'Sword']
+        }
+    else:
+        context['text'] = 'Welcome to Potential Hero, shall we start?'
+
     return HttpResponse(template.render(context))
