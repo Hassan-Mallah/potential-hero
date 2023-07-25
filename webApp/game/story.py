19075e90f94697335011c1f9729674b6a4c4ae31
Hassan <hassansmallah@gmail.com>
2023-07-25 20:54:30 +0200

    rytr api example

--- webApp/game/story.py
+++ webApp/game/story.py
@@ -14,12 +14,80 @@ def get_random_story():
         residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))
 
 
-rytr_api = 'API KEY'
+API_KEY = 'API_KEY'
+API_URL = 'https://api.rytr.me/v1'
 
-url = 'https://api.rytr.me/v1/languages'
 
-headers = {'Authentication': f'Bearer {rytr_api}'}
-res = requests.get(url, headers=headers)
+def get_lanugages():
+    headers = {'Authentication': f'Bearer {API_KEY}'}
+    res = requests.get(f'{API_URL}/languages', headers=headers)
 
-print(res)
-print(res.json())
+    print(res)
+    print(res.json())
+
+
+# Generate content
+def ryte(languageId, toneId, useCaseId, inputContexts):
+    try:
+        data = {
+            'languageId': languageId,
+            'toneId': toneId,
+            'useCaseId': useCaseId,
+            'inputContexts': inputContexts,
+            'variations': 1,
+            'userId': 'USER1',
+            'format': 'html'
+        }
+        headers = {'Authentication': 'Bearer ' + API_KEY}
+        r = requests.post(API_URL + '/ryte', json=data, headers=headers)
+        data = r.json()
+        return data['data']
+    except:
+        print('An exception occurred')
+    return None
+
+
+# Use case detail
+def useCaseDetail(useCaseId='60ed7113732a5b000cf99e8e'):
+    try:
+        headers = {'Authentication': 'Bearer ' + API_KEY}
+        r = requests.get(API_URL + '/use-cases/' + useCaseId, headers=headers)
+        print(r)
+        print(r.content)
+        data = r.json()
+        return data['data']
+    except:
+        print('An exception occurred')
+    return None
+
+
+def get_test():
+    # Step 1 - Identify language ID (use language list API endpoint)
+    languageIdEnglish = '607adac76f8fe5000c1e636d'  # English
+
+    # Step 2 - Identify tone ID (use tone list API endpoint)
+    toneIdConvincing = '60572a639bdd4272b8fe358b'  # Convincing
+
+    if True:
+        # Step 3 - Identify use case ID (use use-case list API endpoint)
+        useCaseIdMagicCommand = '60ed7113732a5b000cf99e8e'  # Magic command
+
+        # Step 4 - Identify use case details (use use-case detail API endpoint)
+        # useCaseMagicCommand = useCaseDetail(useCaseIdMagicCommand)
+
+        key = 'INPUT_TEXT_LABEL'
+        inputContexts = {key: 'Write an email for taking a sick leave'}
+
+        # Step 5 - Generate content (use ryte API endpoint)
+        outputs = ryte(
+            languageIdEnglish,
+            toneIdConvincing,
+            useCaseIdMagicCommand,
+            inputContexts
+        )
+        print(outputs)
+
+
+# get_lanugages()
+# useCaseDetail()
+get_test()
