40895cd363aa1a28edeb925bb45f83a8894b3699
Hassan <hassansmallah@gmail.com>
2023-07-26 23:21:56 +0200

    games weapons

--- webApp/game/views.py
+++ webApp/game/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import HttpResponse, HttpRequest
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
@@ -9,9 +11,12 @@ def index(request: HttpRequest):
     context = {}
 
     if request.POST:
+        # list of game weapons
+        weapons = ['Sword', 'Bow', 'Axe', 'Staff', 'Dagger', 'Gun']
+
         context = {
             'question': 'Which weapon would do you like to use?',
-            'answers': ['Axe', 'Gun', 'Sword']
+            'answers': random.sample(weapons, 3)
         }
     else:
         context['text'] = 'Welcome to Potential Hero, shall we start?'
